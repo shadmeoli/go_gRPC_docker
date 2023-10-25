81a875d7d0d5ad7067f1825de08bd40f9de232e7
shadmeoli <shadrack@shopzetu.com>
2023-10-25 20:14:11 +0300

    feat: home setup

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hadmeoli/go_gRPC_docker
 
 go 1.21.1
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"controller/index"
 	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
 )
+
+func entry(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "")
+}
 
 func main() {
 	fmt.Println("home")
