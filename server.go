6b30313b04c5ddd1b33459c914254d28a0b31508
shadmeoli <shadrack@shopzetu.com>
2023-11-13 16:10:51 +0300

    feat: seting up with htxm for ajax calls to the server

--- server.go
+++ server.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"html/template"
 )
 
 type Note struct {
@@ -26,6 +28,31 @@ func main() {
 	// Middleware
 	app.Use(logger.New())
 
+	// Serve HTML content
+	app.Get("/", func(c *fiber.Ctx) error {
+		// Retrieve all notes from the database
+		var notes []Note
+		db.Find(&notes)
+
+		// Render the HTML template with the notes
+		tmpl, err := template.New("notes").Parse(`<div id="notes-container" class="h-[50%] w-full flex flex-col justify-center items-center rounded-xl p-10">
+            {{range .Notes}}
+                <div class="w-full min-h-20 h-auto bg-gray-800 rounded-lg p-4">
+                    <h1 class="text-center text-gray-400">{{.Text}}</h1>
+                </div>
+            {{else}}
+                <h1 class="text-center text-gray-400 animate-pulse">Notes will appear here</h1>
+            {{end}}
+        </div>`)
+		if err != nil {
+			return err
+		}
+
+		return tmpl.Execute(c.Response().BodyWriter(), struct {
+			Notes []Note
+		}{Notes: notes})
+	})
+
 	// Create a new note
 	app.Post("/api/notes", func(c *fiber.Ctx) error {
 		var note Note
@@ -37,23 +64,21 @@ func main() {
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": result.Error.Error()})
 		}
 
-		return c.Status(fiber.StatusCreated).JSON(note)
-	})
+		// Retrieve all notes from the database
+		var notes []Note
+		db.Find(&notes)
+		fmt.Println("Notes:", notes)
 
-	// Delete a note by ID
-	app.Delete("/api/notes/:id", func(c *fiber.Ctx) error {
-		noteID := c.Params("id")
-		var note Note
+		// Render the HTML template with the updated notes
+		tmpl, err := template.New("notes").Parse(`<!-- Your HTML template here, as shown above -->`)
 
-		if result := db.First(&note, noteID); result.Error != nil {
-			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Note not found"})
+		if err != nil {
+			return err
 		}
 
-		if result := db.Delete(&note); result.Error != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": result.Error.Error()})
-		}
-
-		return c.SendStatus(fiber.StatusOK)
+		return tmpl.Execute(c.Response().BodyWriter(), struct {
+			Notes []Note
+		}{Notes: notes})
 	})
 
 	// Start the Fiber app
